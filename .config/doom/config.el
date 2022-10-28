11d6351abc3c5da22fefbe1cb774043b3b79e068
Vox <voxol.tetra@protonmail.com>
2022-10-28 16:57:06 -0400

    Updated DOOM Emacs config

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -34,6 +34,24 @@
 ;; `load-theme' function. This is the default:
 (setq doom-theme 'doom-gruvbox)
 
+(use-package dashboard
+  :init      ;; tweak dashboard config before loading it
+  (setq dashboard-set-heading-icons t)
+  (setq dashboard-set-file-icons t)
+  ;;(setq dashboard-startup-banner 'logo) ;; use standard emacs logo as banner
+  (setq dashboard-startup-banner "~/.config/doom/blackhole.png")  ;; use custom image as banner
+  (setq dashboard-center-content t) ;; set to 't' for centered content
+;;  (setq dashboard-items '((recents . 5)
+;;                          (agenda . 5 )
+;;                          (bookmarks . 5)
+;;                          (projects . 5)
+;;                          (registers . 5)))
+  :config
+  (dashboard-setup-startup-hook)
+  (dashboard-modify-heading-icons '((recents . "file-text")
+                                    (bookmarks . "book"))))
+
+
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type t)
